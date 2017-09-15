78c5611e86bb4a53c9948b026504697610b1bae4
tawnycat <tawnyeleanor@gmail.com>
2017-09-14 21:05:06 -0400

    added questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,64 @@
+// Objects with questions and answers
+
+var triviaQuestions = {
+	whiskers {
+		question: "A cat has how many whiskers, on average?",
+		answerA: "12",
+		answerB: "16",
+		answerC: "8",
+		answerD: "24"
+	},
+	group {
+		question: "A term for a group of cats is:",
+		answerA: "Clutch",
+		answerB: "Caggle",
+		answerC: "Covey",
+		answerD: "Clowder"
+	},
+	breeds {
+		question: "All of the following are the names of cat breeds, except:",
+		answerA: "Balinese",
+		answerB: "Birman",
+		answerC: "Beauceron",
+		answerD: "Burmilla"
+	},
+	taste {
+		question: "Cats can't taste this:",
+		answerA: "Sour",
+		answerB: "Bitter",
+		answerC: "Salt",
+		answerD: "Sweet"
+	}
+	calico {
+		question: "Calico cats are almost always:",
+		answerA: "Left pawed",
+		answerB: "Female",
+		answerC: "Friendly",
+		answerD: "Finicky"
+	}
+	hemingway {
+		question: "Hemingway cats are felines that have:",
+		answerA: "Written a best-selling book",
+		answerB: "An abnormally large head",
+		answerC: "A cropped tail",
+		answerD: "Extra toes"
+	}
+}
+
+// Loop that will run through each question
+
+// Display first question with answers
+
+// Set timer and display it
+
+// Create logic for right and wrong answers, ran out of time
+
+// Displays "you're correct" page
+
+// Displays "wrong!" page
+
+// Displays "out of time" page 
+
+// Display next question
+
+// Restart timer
